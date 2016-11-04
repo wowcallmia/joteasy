ae94cf827c97b5e352acf2b2f3267bf2e26effe8
John Son <jsson77@gmail.com>
2016-11-03 23:02:06 -0700

    added Home button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import Resources from './Resources';
-import Topics from './Topics';
 import { Header, Segment, Icon } from 'semantic-ui-react';
+import { browserHistory } from 'react-router';
 
 export default class Layout extends Component {
+  goHome () {
+    browserHistory.push('/');
+  }
+
   render () {
     return (
       <Segment inverted>
         <Header inverted as='h2' color='blue' icon textAlign='center'>
-          <Icon name='book' circular />
+          <Icon name='book' onClick={this.goHome.bind(this)} circular />
           <Header.Content>
             JotEasy
           </Header.Content>
         </Header>
         {this.props.children}
-        {/* <Topics />
-        <Resources /> */}
       </Segment>
     );
   }
